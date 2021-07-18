e794e4e8cbca9eaec4794dc3046ee3662e807920
John Trujillo <amriikk@gmail.com>
2021-07-17 22:45:05 -0700

    e

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <GiftedChat
+      messages={messages}
+      placeholder='Hello World'
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
     </View>
   );
 }
