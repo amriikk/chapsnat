2799ff94c2cd973b99df1ff6ed77b7f41f7c1fe0
John Trujillo <amriikk@gmail.com>
2021-07-21 14:38:17 -0700

    revert

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,12 +1,21 @@
-import { Text, View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Image,
+  Alert,
+} from "react-native";
+
+import React, { useState, useEffect } from "react";
 import firebase from "@firebase/app";
 import Colors from "../constants/Colors";
-import React, { useState, useEffect } from "react";
 
-export default function SignupScreen() {
+export default function SignupScreen({ navigation }) {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
 
   const onPressCreate = async () => {
     await firebase
@@ -15,28 +24,27 @@ export default function SignupScreen() {
       .then(onSuccess, onFailure);
   };
 
-  const onSuccess = (userCredential) => {
-    console.log("SUCCESS");
+  const onSuccess = async () => {
+    //Signed in
+    alert("Account created! Please log in.");
     var curr_user = userCredential.user;
     curr_user.updateProfile({
       displayName: name,
     });
   };
 
-  const onFailure = () => {
-    alert("Sign up failure. Please try again.");
+  const onFailure = async () => {
+    alert("Failure! Please try again.");
   };
 
   return (
     <View>
-      <Text>Email:</Text>
-      <TextInput onChangeText={setEmail} />
-
-      <Text>Password (6+ characters):</Text>
-      <TextInput onChangeText={setPassword} secureTextEntry={true} />
-
-      <Text>Name:</Text>
-      <TextInput onChangeText={setName} />
+      <Text style={styles.title}>Email:</Text>
+      <TextInput style={styles.nameInput} onChangeText={setEmail} />
+      <Text style={styles.title}>Password (6+ characters):</Text>
+      <TextInput style={styles.nameInput} onChangeText={setPassword} secureTextEntry={true} />
+      <Text style={styles.title}>Name:</Text>
+      <TextInput style={styles.nameInput} onChangeText={setName} />
 
       <Button
         onPress={onPressCreate}
@@ -47,3 +55,24 @@ export default function SignupScreen() {
     </View>
   );
 }
+
+const offset = 16;
+const styles = StyleSheet.create({
+  title: {
+    marginTop: offset,
+    marginLeft: offset,
+    fontSize: offset,
+  },
+  nameInput: {
+    height: offset * 2,
+    margin: offset,
+    paddingHorizontal: offset,
+    borderColor: "#111111",
+    borderWidth: 1,
+    fontSize: offset,
+  },
+  buttonText: {
+    marginLeft: offset,
+    fontSize: 42,
+  },
+});
